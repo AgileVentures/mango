3870fbea1e76ccd1da74fa271b44336d70b13ff4
mtc2013 <mtc2013opensource@gmail.com>
2017-09-06 09:50:56 -0400

    Commit work from c9 mob

---
+++ config/config.exs
@@ -0,0 +1,31 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :mango,
+  ecto_repos: [Mango.Repo]
+
+# Configures the endpoint
+config :mango, MangoWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "uk9jr8XXXcHpXzympenbHN5GHQi3QBZCy3BKwIyCVdQh4/rLJmagxtelgbWqf1Pf",
+  render_errors: [view: MangoWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Mango.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+  
+# Configure hound  
+config :hound, driver: "phantomjs", host: "https://phoenix-inside-out-tansaku.c9users.io", port: 8082
+ 
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
---
+++ config/test.exs
@@ -0,0 +1,22 @@
+use Mix.Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :mango, MangoWeb.Endpoint,
+  http: [port: 8081],
+  server: true
+
+config :hound, driver: "phantomjs"
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Configure your database
+config :mango, Mango.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "mango_test",
+  hostname: "localhost",
+  template: "template0",
+  pool: Ecto.Adapters.SQL.Sandbox
