eb3f20e003bc702b437cb6cc59cefcaa1d72b76c
federicoesparza <federicoesparza@outlook.com>
2017-10-18 12:10:55 +0000

    Finished section 4.3. Elixir Mob 30

--- lib/mango/catalog/catalog.ex
+++ lib/mango/catalog/catalog.ex
@@ -2,9 +2,11 @@ defmodule Mango.Catalog do
   alias Mango.Catalog.Product
 
   def list_products do
-    product1 = %Product{ name: "Tomato", price: 50, is_seasonal: false, category: "vegetables" }
-    product2 = %Product{ name: "Apple", price: 100, is_seasonal: true, category: "fruits" }
-    [product1, product2]
+    # product1 = %Product{ name: "Tomato", price: 50, is_seasonal: false, category: "vegetables" }
+    # product2 = %Product{ name: "Apple", price: 100, is_seasonal: true, category: "fruits" }
+    # [product1, product2]
+    Product 
+    |> Mango.Repo.all
   end
   
   def list_seasonal_products do
--- lib/mango/catalog/product.ex
+++ lib/mango/catalog/product.ex
@@ -1,3 +1,16 @@
 defmodule Mango.Catalog.Product do
-  defstruct [:name, :price, :is_seasonal, :category]
+  use Ecto.Schema
+  
+  schema "products" do
+      field :name, :string 
+      field :price, :decimal
+      field :sku, :string
+      field :is_seasonal, :boolean, default: false
+      field :image, :string
+      field :pack_size, :string
+      field :category, :string
+  
+      timestamps()
+  end
+  
 end
