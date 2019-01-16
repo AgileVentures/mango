5f547b6c77cc305a884eb7180bb3ed9b8409c97b
federicoesparza <federicoesparza@outlook.com>
2019-01-16 14:30:05 +0000

    Make more progress updating the update function, which could be renamed

--- lib/mango_web/controllers/checkout_controller.ex
+++ lib/mango_web/controllers/checkout_controller.ex
@@ -5,10 +5,13 @@ defmodule MangoWeb.CheckoutController do
   def edit(conn, _params) do
     order           = conn.assigns.cart
     order_changeset = Sales.change_cart(order)
+    # conn.assign(conn, :order, order)
     render conn, "edit.html", order: order, order_changeset: order_changeset
   end
   
   def update(conn, _params) do
-    conn
+    order           = conn.assigns.cart
+    Sales.update_cart(order, %{garbage_key: "I'm garbage!"})
+    render conn, "edit.html"
   end
 end
