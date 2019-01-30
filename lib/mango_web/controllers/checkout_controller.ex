d634cd1e8e5823d6208efe229810b65c04fa72a8
federicoesparza <federicoesparza@outlook.com>
2019-01-30 14:11:03 +0000

    Add put_customer_data_in_params function

--- lib/mango_web/controllers/checkout_controller.ex
+++ lib/mango_web/controllers/checkout_controller.ex
@@ -5,23 +5,31 @@ defmodule MangoWeb.CheckoutController do
   def edit(conn, _params) do
     order           = conn.assigns.cart
     order_changeset = Sales.change_cart(order)
-    # conn.assign(conn, :order, order)
+    # conn.assign(conn, :order, order) <-- why?
     render conn, "edit.html", order: order, order_changeset: order_changeset
   end
   
   def update(conn, %{"order" => order_params}) do
     order           = conn.assigns.cart
-    order_changeset = Sales.confirm_order(order, order_params)
-    # Sales.update_cart(order, %{garbage_key: "I'm garbage!"})
+    order_params = put_customer_data_in_params(conn, order_params)
     case Sales.confirm_order(order, order_params) do
       {:ok, _} ->
         conn
         |> put_flash(:info, "Your order has been confirmed")
-        |> redirect_to "/"
+        |> redirect(to: "/")
       {:error, order_changeset} -> 
         conn
         |> put_flash(:error, "Problem updating")
-        |> render conn, "edit.html", order_changeset: order_changeset
+        |> render("edit.html", order_changeset: order_changeset, order: order)
     end
+  end
+  
+  def put_customer_data_in_params(conn, params) do
+    customer = conn.assigns.current_customer
+    params
+    |> Map.put("customer_id", customer.id)
+    |> Map.put("customer_name", customer.name)
+    |> Map.put("customer_residence_area", customer.residence_area)
+    |> Map.put("email", customer.email)
   end
 end
